c95b12d533977601939bb41bb5fb52a1776a1595
Marcosauras <44006538+Marcosauras@users.noreply.github.com>
2022-12-09 17:19:43 -0500

    added local storage to grab zipcode, still has bugs

--- script.js
+++ script.js
@@ -2,3 +2,33 @@
 // make button to go back to zipcode
 // make buttons stay even if you refresh page
 
+let usersZipCode = [];
+let pastSearchZipCodes = document.querySelector(".userLastZipCode")
+const text = document.querySelector('text');
+
+addEventListener('submit', event => {
+let zipCodeInput = document.querySelector("#zip-code").value.trim();
+// text.textContent = number.target.value
+saveZipCode(zipCodeInput);
+oldZipCodes(zipCodeInput)
+})
+
+var saveZipCode = function (zipCodeInput) {
+localStorage.setItem("zipCode", JSON.stringify(zipCodeInput))
+}
+
+var oldZipCodes = function(e) {
+var storedZipCodes = JSON.parse(localStorage.getItem("zipCode"));
+console.log(storedZipCodes)
+
+    pastSearchEl = document.createElement("button");
+    pastSearchEl.textContent = storedZipCodes;
+    pastSearchEl.classList = "zip-code-btn";
+    pastSearchEl.setAttribute("data-zipCode", storedZipCodes);
+    pastSearchEl.setAttribute("type", "submit");
+    pastSearchZipCodes.prepend(pastSearchEl)
+
+}
+oldZipCodes();
+
+document.querySelector('.zip-code-btn').addEventListener("click", pastZipCodeSubmitHandler)
