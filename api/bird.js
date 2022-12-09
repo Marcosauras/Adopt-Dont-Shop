60d4795816921d69c6a6d62d574047df55ce3fb6
Marcosauras <44006538+Marcosauras@users.noreply.github.com>
2022-12-09 16:03:03 -0500

    styled animal cards

--- api/bird.js
+++ api/bird.js
@@ -49,43 +49,48 @@ function showAnimals(pet) {
     pet.forEach(pet => {
         const div = document.createElement("div");
 
-        div.classList.add("card", "card-body", "bg-stone-300", "bg-opacity-50");
+        div.classList.add("card", "card-body", "bg-stone-300", "bg-opacity-50", "lg:max-w-4xl", "lg:flex", "justify-center");
         div.innerHTML = `
-      <div class="p-5">
-        <div class="pet-container block sm:flex">
-        <img class="pet-image mb-5 mr-5 w-full max-h-32 sm:w-auto" src="${
+      <div class="p-5 pet-card ">
+        <div class="pet-container block contents md:flex md:justify-start">
+        <img class="pet-image mb-5 mr-5 max-h-96" src="${
           pet.photos[0] ? pet.photos[0].medium : ""
         }">
           <div  class="mb-5">
-            <ul class="">
-            <li class=""><strong>Name: </strong>${pet.name} (${pet.age})</li>
-            <li  class="text-secondary"><strong>Breed: </strong>${pet.breeds.primary}</li>
-              <li><strong>Location: </strong>${pet.contact.address.city}, ${pet.contact.address.state} ${
-                pet.contact.address.postcode},${pet.contact.address.country} </li>
-              <li class="">${
-                pet.contact.phone
-                  ? `<li class=""><strong>Phone: </strong>${pet.contact.phone}</li>`
-                  : ``}</li>
-              ${
-                pet.contact.email
-                  ? `<li class=""><strong>Email: </strong>${pet.contact.email}</li>`
-                  : ``
-              }
-              <li class=""><strong>Shelter ID: </strong>${pet.organization_id}</li>
-            </ul>
-            <a class="font-extrabold" href="${pet.url}" target="_blank">Click Here For more info</a>
+            <div>
+              <ul class="">
+              <li class=""><strong>Name: </strong>${pet.name} (${pet.age})</li>
+              <li  class="text-secondary"><strong>Breed: </strong>${pet.breeds.primary}</li>
+                <li><strong>Location: </strong>${pet.contact.address.city}, ${pet.contact.address.state} ${
+                  pet.contact.address.postcode},${pet.contact.address.country} </li>
+                <li class="">${
+                  pet.contact.phone
+                    ? `<li class=""><strong>Phone: </strong>${pet.contact.phone}</li>`
+                    : ``}</li>
+                ${
+                  pet.contact.email
+                    ? `<li class=""><strong>Email: </strong>${pet.contact.email}</li>`
+                    : ``
+                }
+                <li class=""><strong>Shelter ID: </strong>${pet.organization_id}</li>
+              </ul>
+              <a class="font-extrabold" href="${pet.url}" target="_blank">Click Here For more info</a>
+            </div>
+            <div>
+              <iframe
+                class = "max-w-96 max-h-96"
+                style="border:0"
+                loading="lazy"
+                allowfullscreen
+                referrerpolicy="no-referrer-when-downgrade"
+                src="https://www.google.com/maps/embed/v1/place?key=AIzaSyC7PriPa6496JhFyFBICpjkcjkxgyUukgM
+                &q=${pet.contact.address.city}+${pet.contact.address.state}+${
+                  pet.contact.address.postcode}">
+              </iframe>
+            </div>
           </div>
         </div>
-        <iframe class="h-32 sm:h-64"
-         width="100%"
-         style="border:0"
-         loading="lazy"
-         allowfullscreen
-         referrerpolicy="no-referrer-when-downgrade"
-         src="https://www.google.com/maps/embed/v1/place?key=AIzaSyC7PriPa6496JhFyFBICpjkcjkxgyUukgM
-         &q=${pet.contact.address.city}+${pet.contact.address.state}+${
-          pet.contact.address.postcode}">
-        </iframe>
+        
             
 
         
